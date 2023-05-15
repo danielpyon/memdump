98c5950669d24560687435d9401c211c354e77be
Daniel Pyon <pyondohyun@gmail.com>
2023-05-15 00:03:33 -0700

    remove cctor, op=

--- memlib.h
+++ memlib.h
@@ -11,7 +11,8 @@ class MemoryTool {
   public:
     MemoryTool(pid_t pid) : _pid(pid) { };
     ~MemoryTool() { };
-    MemoryTool(const MemoryTool& rhs) = delete;
+    MemoryTool(const MemoryTool& other) = delete;
+    MemoryTool& operator=(const MemoryTool& rhs) = delete;
 
     bool ReadMem(char* start, const uint32_t len,
                  std::unique_ptr<char*>* result);
